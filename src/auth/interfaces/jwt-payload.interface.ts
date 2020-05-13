e7e5e56d0d75b1daa500aa8c869e123def9e4ab7
James Liauw <jamesliauw@163.com>
2020-05-13 15:29:51 +0800

    feat: add common

--- src/auth/interfaces/jwt-payload.interface.ts
+++ src/auth/interfaces/jwt-payload.interface.ts
@@ -1,6 +1,6 @@
 export interface JwtPayload {
-    id: number;
-    email: string;
-    firstName: string;
-    lastName: string;
-  }
+	id: number;
+	email: string;
+	firstName: string;
+	lastName: string;
+}
