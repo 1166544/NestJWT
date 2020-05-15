38d800c5008fb36a61955f88465a5a6ca2a4964d
James Liauw <jamesliauw@163.com>
2020-05-15 09:24:59 +0800

    feat: add common

--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -1,18 +1,18 @@
 import { ApiHideProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
-    @ApiHideProperty()
-    readonly id: number;
+	@ApiHideProperty()
+	readonly id: number;
 
-    @ApiHideProperty()
-    readonly firstName: string;
+	@ApiHideProperty()
+	readonly firstName: string;
 
-    @ApiHideProperty()
-    readonly lastName: string;
+	@ApiHideProperty()
+	readonly lastName: string;
 
-    @ApiHideProperty()
-    readonly email: string;
+	@ApiHideProperty()
+	readonly email: string;
 
-    @ApiHideProperty()
-    readonly password: string;
+	@ApiHideProperty()
+	readonly password: string;
 }
