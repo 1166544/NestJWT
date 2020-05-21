aa12dcc259730ac68bff9f6d9911e149b1ac7fb4
James Liauw <jamesliauw@163.com>
2020-05-21 16:27:12 +0800

    feat: add common

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,57 +10,57 @@ import { CreateUserDto } from 'src/users/users.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+	constructor(private readonly usersService: UsersService) {}
 
-  private readonly logger = new Logger(AuthService.name);
-  
-  async register(user: CreateUserDto) {
-    let status: RegistrationStatus = {
-      success: true,
-      message: 'user register',
-    };
-    try {
-      await this.usersService.register(user);
-    } catch (err) {
-      //debug(err);
-      status = { success: false, message: err };
-    }
-    return status;
-  }
-  createToken(user: User) {
-    //debug('get the expiration');
-    const expiresIn = 3600;
-    //debug('sign the token');
-    //debug(user);
+	private readonly logger = new Logger(AuthService.name);
 
-    const accessToken = jwt.sign(
-      {
-        id: user.id,
-        email: user.email,
-        firstname: user.firstName,
-        lastname: user.lastName,
-      },
-      'Codebrains',
-      { expiresIn },
-    );
-    //debug('return the token');
-    //debug(accessToken);
-    return {
-      expiresIn,
-      accessToken,
-    };
-  }
+	async register(user: CreateUserDto) {
+		let status: RegistrationStatus = {
+			success: true,
+			message: 'user register'
+		};
+		try {
+			await this.usersService.register(user);
+		} catch (err) {
+			//debug(err);
+			status = { success: false, message: err };
+		}
+		return status;
+	}
+	createToken(user: User) {
+		//debug('get the expiration');
+		const expiresIn = 3600;
+		//debug('sign the token');
+		//debug(user);
 
-  async validateUserToken(payload: JwtPayload): Promise {
-    return await this.usersService.findById(payload.id);
-  }
-  async validateUser(email: string, password: string): Promise {
-    const user = await this.usersService.findByEmail(email);
-    if (user && user.comparePassword(password)) {
-      this.logger.log('password check success');
-      const { password, ...result } = user;
-      return result;
-    }
-    return null;
-  }
+		const accessToken = jwt.sign(
+			{
+				id: user.id,
+				email: user.email,
+				firstname: user.firstName,
+				lastname: user.lastName
+			},
+			'Codebrains',
+			{ expiresIn }
+		);
+		//debug('return the token');
+		//debug(accessToken);
+		return {
+			expiresIn,
+			accessToken
+		};
+	}
+
+	async validateUserToken(payload: JwtPayload): Promise {
+		return await this.usersService.findById(payload.id);
+	}
+	async validateUser(email: string, password: string): Promise {
+		const user = await this.usersService.findByEmail(email);
+		if (user && user.comparePassword(password)) {
+			this.logger.log('password check success');
+			const { password, ...result } = user;
+			return result;
+		}
+		return null;
+	}
 }
