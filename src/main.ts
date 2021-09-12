06a92f291fa9664fe5e2920b84faafdc12cc9a36
williamliao <jamesliauw@163.com>
2021-09-12 11:16:44 +0800

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const port = 3000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  // swagger
+  const options = new DocumentBuilder()
+    .setTitle('Nest API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
