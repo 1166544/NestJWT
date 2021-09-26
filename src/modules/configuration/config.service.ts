965c68d886696d320edf14eea8d46d0a2ae63da8
william liauw <jamesliauw@163.com>
2021-09-26 15:17:45 +0800

    feat: add config

--- src/modules/configuration/config.service.ts
+++ src/modules/configuration/config.service.ts
@@ -4,7 +4,15 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { CONFIG_OPTIONS } from './constants';
 import { ConfigOptions, EnvConfig } from './interfaces';
+import cacheManager from 'cache-manager';
+import redisStore from 'cache-manager-redis-store';
 
+/**
+ * 配置服务
+ * 
+ * @export
+ * @class ConfigService
+ */
 @Injectable()
 export class ConfigService {
   private readonly envConfig: EnvConfig;
@@ -16,10 +24,46 @@ export class ConfigService {
     this.envConfig = dotenv.parse(fs.readFileSync(envFile));
 
     // connect redis
-    
+
   }
 
   get(key: string): string {
     return this.envConfig[key];
+  }
+
+  /**
+   * 从缓存中获取内容
+   * 
+   * @param {string} key 
+   * @returns {string} 
+   * 
+   * @memberOf ConfigService
+   */
+  public getCache(key: string): string {
+    return key;
+  }
+
+  /**
+   * 设置缓存
+   * 
+   * @param {string} key 
+   * @param {*} value 
+   * 
+   * @memberOf ConfigService
+   */
+  public setCache(key: string, value: any): void {
+    // hole
+  }
+
+  /**
+   * 移除缓存
+   * 
+   * @param {string} key 
+   * @returns {boolean} 
+   * 
+   * @memberOf ConfigService
+   */
+  public delCahe(key: string): boolean {
+    // hole
   }
 }
