86f0a43a0f4d520c6381ec7958c78c17f155716a
william liauw <jamesliauw@163.com>
2021-09-28 16:23:17 +0800

    feat: add common

--- src/modules/cats/CatsModule.ts
+++ src/modules/cats/CatsModule.ts
@@ -2,6 +2,12 @@ import { Module } from '@nestjs/common';
 import { CatsController } from './CatsController';
 import { CatsService } from './CatsService';
 
+/**
+ * demo module
+ * 
+ * @export
+ * @class CatsModule
+ */
 @Module({
   controllers: [CatsController],
   providers: [CatsService],
